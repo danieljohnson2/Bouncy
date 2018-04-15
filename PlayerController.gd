f2b0dc7324d9e36b97235e7cd5535f288186a9a9
Dan Johnson <danieljohnson2@verizon.net>
2018-04-15 09:07:21 -0400

    Added "Q" as quit command, also tweaked speeds to add a bit of challenge- the evil ball will start moving faster over time

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,6 +1,6 @@
 extends Node
 
-export var responsiveness = 100
+export var responsiveness = 250
 
 var directions = {
 	"ui_right": Vector2(1,0),
@@ -11,6 +11,9 @@ var directions = {
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
 	# Update game logic here.
+	
+	if Input.is_action_pressed("ui_quit"):
+		get_tree().quit()
 	
 	var ball = self.get_parent()
 	
