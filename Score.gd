5edd5e87bb039dc18419e6c6117f4c206ac72dd1
Dan Johnson <danieljohnson2@verizon.net>
2018-04-15 09:27:30 -0400

    Added a score, really a timer for how long you could dodge the evil balls

---
+++ Score.gd
@@ -0,0 +1,10 @@
+extends Label
+
+var score = 0
+
+func _process(delta):
+	var player = get_tree().get_nodes_in_group("player")[0]
+	
+	if player.visible:
+		score += delta
+		self.text = "Score: %0.2f seconds" % score
