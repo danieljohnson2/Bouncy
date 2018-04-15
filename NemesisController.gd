5af229a94347a5b09cccb34e417ffe2cded8422a
Dan Johnson <danieljohnson2@verizon.net>
2018-04-15 09:15:00 -0400

    Triple threat! Use an instanced scene to have 3 evil balls.

---
+++ NemesisController.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var change_size = 50
+var until_change = 0
+
+func _process(delta):
+	until_change -= delta
+	
+	if until_change <= 0:
+		until_change = rand_range(1, 3)
+		var ball = get_parent()
+		var offset = Vector2(rand_range(-change_size, change_size), rand_range(change_size, change_size))
+		change_size *= 1.25
+		ball.speed += offset
