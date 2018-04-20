d8c76f01c6a7f2b04822d1b1a56dd94a93962bc0
Dan Johnson <danieljohnson2@verizon.net>
2018-04-20 16:48:51 -0400

    Improved controls using a normalized vector and tweaked acceleration values.

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends RigidBody2D
 
-export var acceleration = 250
-export var fast_acceleration = 750
+export var acceleration = 200
+export var speed_factor = 3.0
+
 export (NodePath) var die_player_path
 
 # The movement controls represented as normalized vectors;
@@ -13,6 +14,23 @@ var directions = {
 	"ui_up": Vector2(0,-1),
 	"ui_down": Vector2(0,1) }
 
+# Provides a vector2 that indicates how the player should
+# move; this vector is normalized, to avoid excess speed
+# if the player hits two arrows at once. But if the shift
+# key is help, we scale the result up.
+func get_input_direction():
+	var movement = Vector2(0,0)
+	
+	# Can't really use a dictionary lookup here since more than one
+	# action can be engaged, so we just try each one.
+	for action in directions.keys():
+		if Input.is_action_pressed(action):
+			movement += directions[action]
+	
+	movement = movement.normalized()
+	if Input.is_action_pressed("ui_fast"): movement *= speed_factor
+	return movement
+	
 func _process(delta):
 	# Quit, Restart
 	if Input.is_action_pressed("ui_quit"):
@@ -24,15 +42,11 @@ func _process(delta):
 	# Stop isn't too subtle, but this works regardless of delta
 	if Input.is_action_pressed("ui_stop"):
 		self.linear_velocity = Vector2(0,0)
-		
-	# Can't really use a dictionary lookup here since more than one
-	# action can be engaged, so we just try each one.
-	# TODO: Combine the directions and normalize so mulit-keypress
-	# doesn't get you more speed
-	for action in directions.keys():
-		if Input.is_action_pressed(action):
-			var acc = fast_acceleration if Input.is_action_pressed("ui_fast") else acceleration
-			self.apply_impulse(Vector2(), directions[action] * acc * delta)
+	
+	var movement = get_input_direction()
+	
+	if movement != Vector2(0,0):
+		self.apply_impulse(Vector2(), movement * acceleration * delta)
 	
 	self.rotation = 0 # damping isn't always effective
 
@@ -45,6 +59,7 @@ func _on_Ball_body_entered(body):
 		if not die_player.is_playing():
 			 die_player.play("Fade Out")
 
+# Called from an animation when the player has finished dieing
 func game_over():
 	self.visible = false
 	for go in get_tree().get_nodes_in_group("gameover"):
